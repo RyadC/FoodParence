04d261bb4b30989a9fe6476d34331fa865e70f36
RyadC <r.chair@hotmail.fr>
2024-07-25 18:13:37 +0200

    :sparkles: feat(root): add root application

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/Main.jsx";
+import Functioning from "./components/Functioning.jsx";
+import Root from "./components/Root.jsx";
+import Header from "./components/Header.jsx";
+import Ressources from "./components/Ressources.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Main />} />
+          <Route path="/fonctionnement" element={<Functioning />} />
+          <Route path="/ressources" element={<Ressources />} />
+        </Route>
+      </Routes>
+      {/* <Main></Main> */}
+      {/* <Footer></Footer> */}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
