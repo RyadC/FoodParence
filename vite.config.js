78ecfb549f65d90d1e1fbc81bfe11c51ef461158
RyadC <r.chair@hotmail.fr>
2024-07-25 16:43:03 +0200

    :package: build(docker): Add docker and docker-compose for development environment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // preview: {
+  // port: 5173,
+  // strictPort: true,
+  // },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    // strictPort: true,
+    // origin: "http://0.0.0.0:5173",
+    watch: {
+      usePolling: true,
+    },
+  },
+});
