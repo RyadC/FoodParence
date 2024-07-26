7ca38cf5c188fa2bc983605ea56a90892efc707d
RyadC <r.chair@hotmail.fr>
2024-07-26 06:02:28 +0200

    :sparkles: feat(Main): add SearchForm in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,22 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/Groupe 1.png";
+import SearchForm from "./SearchForm";
+
 const Main = () => {
   return (
     <section className="home">
-      <h1 className="home-h1">
-        <a className="home-h1-link" href="#0">
-          <img
-            className="home-h1-img"
-            src={logo}
-            alt="Le Logo de Food'Parence"
-          />
-        </a>
+      <h1 className="home__h1">
+        <Link className="home__link" to="/">
+          <img className="home__img" src={logo} alt="Le Logo de Food'Parence" />
+        </Link>
       </h1>
 
-      <p className="home-slogan">
-        Exigez plus de{" "}
-        <strong className="slogan-important">transparence</strong> pour vos{" "}
-        <strong className="slogan-important">produits</strong>
+      <p className="home__slogan">
+        Exigez plus de <strong className="important">transparence</strong> pour
+        vos <strong className="important">produits</strong>
       </p>
 
-      <form
-        id="form"
-        // onSubmit={(e) => {
-        //   e.preventDefault();
-        //   launchSearch();
-        //   jsProduct();
-        // }}
-      >
-        {/* <BarCodeSearch onInputChange={onInputChange} /> */}
-        <div className="search-detailsProduct">
-          <div className="search-detailsProduct-container">
-            {/* <CheckboxSearch checkboxName={'Allergènes'} nameProp={'allergen'} handleCheckbox={handleCheckbox} />
-          <CheckboxSearch checkboxName={'Additifs'} nameProp={'additive'} handleCheckbox={handleCheckbox} />
-          <CheckboxSearch checkboxName={'Non Halal'} nameProp={'no-halal'} handleCheckbox={handleCheckbox} /> */}
-          </div>
-        </div>
-      </form>
+      <SearchForm />
     </section>
   );
 };
