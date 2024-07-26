6f0c62a64ffe00f2ccf087dedb123f11cdcb7cf7
RyadC <r.chair@hotmail.fr>
2024-07-26 06:11:39 +0200

    :lipstick: style(Footer): add html footer and change css classname

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <></>;
+  return (
+    <footer className="footer">
+      <div className="brand">
+        <span className="brand__text">FoodParence</span>
+        <span className="brand__copyright">&copy;</span>
+      </div>
+
+      <div className="agency">
+        <div className="agency__box">
+          <span className="agency__text">Neiyad Agency</span>
+          <span className="agency__copyright">&copy;</span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
